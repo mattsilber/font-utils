sgaikar1/FontUtili

four new widgets added to library

fontutils/src/main/java/com/appnirman/fontutils/CheckedTextView.java
@@ -1,0 +1,44 @@
+package com.appnirman.fontutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.appnirman.fontutils.fonts.FontUtils;
+
+
+public class CheckedTextView extends android.widget.CheckedTextView {
+
+    public CheckedTextView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public CheckedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public CheckedTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public CheckedTextView init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

fontutils/src/main/java/com/appnirman/fontutils/MultiAutoCompleteTextView.java
@@ -1,0 +1,44 @@
+package com.appnirman.fontutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.appnirman.fontutils.fonts.FontUtils;
+
+
+public class MultiAutoCompleteTextView extends android.widget.MultiAutoCompleteTextView {
+
+    public MultiAutoCompleteTextView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public MultiAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public MultiAutoCompleteTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public MultiAutoCompleteTextView init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

fontutils/src/main/java/com/appnirman/fontutils/Switch.java
@@ -1,0 +1,47 @@
+package com.appnirman.fontutils;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+
+import com.appnirman.fontutils.fonts.FontUtils;
+
+
+@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class Switch extends android.widget.Switch {
+
+    public Switch(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public Switch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public Switch(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public Switch init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

fontutils/src/main/java/com/appnirman/fontutils/ToggleButton.java
@@ -1,0 +1,44 @@
+package com.appnirman.fontutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.appnirman.fontutils.fonts.FontUtils;
+
+
+public class ToggleButton extends android.widget.ToggleButton {
+
+    public ToggleButton(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public ToggleButton init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

mattsilber/font-utils

src/main/java/com/guardanis/fontutils/CheckedTextView.java
@@ -1,0 +1,44 @@
+package com.guardanis.fontutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.guardanis.fontutils.fonts.FontUtils;
+
+
+public class CheckedTextView extends android.widget.CheckedTextView {
+
+    public CheckedTextView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public CheckedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public CheckedTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public CheckedTextView init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

src/main/java/com/guardanis/fontutils/MultiAutoCompleteTextView.java
@@ -1,0 +1,44 @@
+package com.guardanis.fontutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.guardanis.fontutils.fonts.FontUtils;
+
+
+public class MultiAutoCompleteTextView extends android.widget.MultiAutoCompleteTextView {
+
+    public MultiAutoCompleteTextView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public MultiAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public MultiAutoCompleteTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public MultiAutoCompleteTextView init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

src/main/java/com/guardanis/fontutils/Switch.java
@@ -1,0 +1,46 @@
+package com.guardanis.fontutils;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+
+import com.guardanis.fontutils.fonts.FontUtils;
+
+@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class Switch extends android.widget.Switch {
+
+    public Switch(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public Switch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public Switch(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public Switch init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}

src/main/java/com/guardanis/fontutils/ToggleButton.java
@@ -1,0 +1,44 @@
+package com.guardanis.fontutils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.guardanis.fontutils.fonts.FontUtils;
+
+
+public class ToggleButton extends android.widget.ToggleButton {
+
+    public ToggleButton(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ToggleButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    public ToggleButton init(AttributeSet attrs, int defStyle) {
+        setWillNotDraw(false);
+
+        FontUtils.getInstance(getContext())
+                .load(this, attrs, defStyle);
+
+        return this;
+    }
+
+    public void setFont(int fontFamilyResId, int style) {
+        setFont(getResources().getString(fontFamilyResId), style);
+    }
+
+    public void setFont(String fontFamily, int style) {
+        FontUtils.getInstance(getContext())
+                .setTypeface(this, fontFamily, style);
+    }
+
+}